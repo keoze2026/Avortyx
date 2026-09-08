059762f905d66418e54fbd5aa3143dc6a733f9dd
morimura07 <morimuradin@gmail.com>
2026-09-09 00:52:22 +0900

    fix: font size

--- components/app-shell/topbar.tsx
+++ components/app-shell/topbar.tsx
@@ -186,7 +186,7 @@ function TopStat({ label, value, tone, icon: Icon }: TopStatProps) {
       </span>
       <span
         className={cn(
-          "text-[15px] font-bold leading-none tabular-nums",
+          "text-[13px] font-bold leading-none tabular-nums",
           TONE_TEXT[tone],
         )}
       >
